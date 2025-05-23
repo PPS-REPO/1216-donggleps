a5cdd911bf4d12686b901fb6efb096fdc5c9c18d
pps-admin <pps.github@gmail.com>
2025-05-23 20:43:29 +0900

    Update: path 'generator/generator.cc' - Updated by 'notice99'

--- generator/generator.cc
+++ generator/generator.cc
@@ -1,20 +1,33 @@
 #include "testlib.h"
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, char** argv) {
-    int al, ar, bl, br, a, b;
-    
+int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
+    const int MIN_VAL = 1;
+    const int MAX_VAL = 1000000000;
 
-    al = atoi(argv[1]);
-    ar = atoi(argv[2]);
-    bl = atoi(argv[3]);
-    br = atoi(argv[4]);
+    bool canFormTriangle = rnd.next(0, 1);
 
-    a = rnd.next(al, ar);
-    b = rnd.next(bl, br);
+    int a = rnd.next(MIN_VAL, MAX_VAL);
+    int b = rnd.next(MIN_VAL, MAX_VAL);
+    int c;
 
-    printf("%d %d\n", a, b);
+    if (canFormTriangle) {
+        long long sum = (long long)a + (long long)b;
+        int upperBound = (sum - 1 <= MAX_VAL) ? (int)(sum - 1) : MAX_VAL;
+        c = rnd.next(MIN_VAL, upperBound);
+    } else {
+        long long sum = (long long)a + (long long)b;
+        if (sum > MAX_VAL) {
+            c = rnd.next(MIN_VAL, MAX_VAL);
+        } else {
+            c = rnd.next((int)sum, MAX_VAL);
+        }
+    }
+
+    cout << a << " " << b << " " << c << "\n";
+
     return 0;
 }
