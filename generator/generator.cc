75128ec143eda6375b03c7c77f09a036b70608a1
pps-admin <pps.github@gmail.com>
2025-05-23 21:03:20 +0900

    Update: path 'generator/generator.cc' - Updated by 'notice99'

--- generator/generator.cc
+++ generator/generator.cc
@@ -6,25 +6,26 @@ using namespace std;
 int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
     const int MIN_VAL = 1;
-    const int MAX_VAL = 1000000000;
+    const long long MAX_VAL = 1000000000LL;
 
     bool canFormTriangle = rnd.next(0, 1);
 
-    int a = rnd.next(MIN_VAL, MAX_VAL);
-    int b = rnd.next(MIN_VAL, MAX_VAL);
-    int c;
-
+    int a = rnd.next(MIN_VAL, (int)MAX_VAL);
+    int b = rnd.next(MIN_VAL, (int)MAX_VAL);
     long long sum = (long long)a + (long long)b;
 
+    int c;
+
     if (canFormTriangle) {
-        int upperBound = (sum - 1 <= MAX_VAL) ? (int)(sum - 1) : MAX_VAL;
+        // c < a+b
+        int upperBound = (sum - 1 <= MAX_VAL) ? (int)(sum - 1) : (int)MAX_VAL;
         c = rnd.next(MIN_VAL, upperBound);
     } else {
+        // c >= a+b or sum overflow
         if (sum > MAX_VAL) {
-            c = rnd.next(MIN_VAL, MAX_VAL);
+            c = rnd.next(MIN_VAL, (int)MAX_VAL);
         } else {
-            // c >= a+b
-            c = rnd.next((int)sum, MAX_VAL);
+            c = rnd.next((int)sum, (int)MAX_VAL);
         }
     }
 
